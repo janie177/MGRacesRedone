1697bf5a7fa2e744e2a6fd1c8dd1cfecbc07f35d
Jan <janie177@hotmail.com>
2014-12-27 19:53:07 +0100

    Stop Aurora's from drowning to a horible death.

--- src/main/java/com/minegusta/mgracesredone/listeners/racelisteners/AuroraListener.java
+++ src/main/java/com/minegusta/mgracesredone/listeners/racelisteners/AuroraListener.java
@@ -1,6 +1,26 @@
 package com.minegusta.mgracesredone.listeners.racelisteners;
 
+import com.minegusta.mgracesredone.races.RaceType;
+import com.minegusta.mgracesredone.util.Races;
+import com.minegusta.mgracesredone.util.WorldCheck;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 
-public class AuroraListener implements Listener{
+public class AuroraListener implements Listener
+{
+    @EventHandler
+    public void onDrown(EntityDamageEvent e)
+    {
+        if(e.getEntity() instanceof Player && e.getCause() == EntityDamageEvent.DamageCause.DROWNING)
+        {
+            if(!WorldCheck.isEnabled(e.getEntity().getWorld()))return;
+            Player p = (Player) e.getEntity();
+            if(Races.getRace(p) == RaceType.AURORA)
+            {
+                e.setCancelled(true);
+            }
+        }
+    }
 }
