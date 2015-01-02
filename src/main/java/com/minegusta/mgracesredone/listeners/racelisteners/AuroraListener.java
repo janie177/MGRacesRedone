56a50e3e9f0d67922fdbd04a9158e29b61bd2842
Jan <janie177@hotmail.com>
2015-01-02 01:17:10 +0100

    Snow now blocks fall damage.

--- src/main/java/com/minegusta/mgracesredone/listeners/racelisteners/AuroraListener.java
+++ src/main/java/com/minegusta/mgracesredone/listeners/racelisteners/AuroraListener.java
@@ -46,8 +46,9 @@ public class AuroraListener implements Listener
             if(isAurora(p))
             {
                 Material mat = p.getLocation().getBlock().getRelative(BlockFace.DOWN).getType();
+                Material mat2 = p.getLocation().getBlock().getType();
 
-                if(!snowBlocks.contains(mat))return;
+                if(!snowBlocks.contains(mat) || !snowBlocks.contains(mat2))return;
                 e.setDamage(0.0);
                 e.setCancelled(true);
             }
