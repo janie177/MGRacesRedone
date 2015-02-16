56cea08e6e1fd340b80ff636740c13de4271e041
Jan <janie177@hotmail.com>
2015-02-16 18:03:18 +0100

    Fixed nullpointer exception for items without any meta data.

--- src/main/java/com/minegusta/mgracesredone/util/ItemUtil.java
+++ src/main/java/com/minegusta/mgracesredone/util/ItemUtil.java
@@ -98,7 +98,7 @@ public class ItemUtil
 
         if(is1.getType().equals(is2.getType()))
         {
-            if((!is1.getItemMeta().hasLore() && !is2.getItemMeta().hasLore()) || is1.getItemMeta().getLore().equals(is2.getItemMeta().getLore()))
+            if((!is1.hasItemMeta() && !is2.hasItemMeta()) || (!is1.getItemMeta().hasLore() && !is2.getItemMeta().hasLore()) || is1.getItemMeta().getLore().equals(is2.getItemMeta().getLore()))
             {
                 if((!is1.getItemMeta().hasDisplayName() && !is2.getItemMeta().hasDisplayName()) || is1.getItemMeta().getDisplayName().equals(is2.getItemMeta().getDisplayName()))
                 {
