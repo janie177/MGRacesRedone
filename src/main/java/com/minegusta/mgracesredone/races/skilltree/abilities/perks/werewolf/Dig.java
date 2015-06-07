536ecb1d372fd7fd3d1dd4bd109dec3d210a001b
Jan <janie177@hotmail.com>
2015-06-07 05:05:42 +0200

    Fixed the nocheat delay.

--- src/main/java/com/minegusta/mgracesredone/races/skilltree/abilities/perks/werewolf/Dig.java
+++ src/main/java/com/minegusta/mgracesredone/races/skilltree/abilities/perks/werewolf/Dig.java
@@ -106,7 +106,7 @@ public class Dig implements IAbility {
                     permission.playerRemoveTransient(p, "nocheatplus.checks.blockbreak.wrongblock");
 
                 }
-            }, face.ordinal() * 5);
+            }, face.ordinal() + 3);
         }
     }
 
