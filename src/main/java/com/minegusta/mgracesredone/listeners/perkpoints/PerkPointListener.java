77e44cb88544309132215ce1d7b66c1888b566a6
Grant <GRANTSWIM4@users.noreply.github.com>
2015-06-25 20:46:11 -0400

    Fixed to address HQM concerns

--- src/main/java/com/minegusta/mgracesredone/listeners/perkpoints/PerkPointListener.java
+++ src/main/java/com/minegusta/mgracesredone/listeners/perkpoints/PerkPointListener.java
@@ -16,60 +16,67 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import java.util.concurrent.ConcurrentMap;
 
 public class PerkPointListener implements Listener {
-    public static ConcurrentMap<String, KillData> killTracker = Maps.newConcurrentMap();
+	public static ConcurrentMap<String, KillData> killTracker = Maps.newConcurrentMap();
 
-    @EventHandler
-    public void onPerkAward(PlayerDeathEvent e) {
-        if (!WorldCheck.isEnabled(e.getEntity().getWorld())) return;
+	@EventHandler
+	public void onPerkAward(PlayerDeathEvent e) {
+		if (!WorldCheck.isEnabled(e.getEntity().getWorld()))
+			return;
 
-        EntityDamageEvent damageEvent = e.getEntity().getLastDamageCause();
-        if (damageEvent != null && damageEvent instanceof EntityDamageByEntityEvent) {
-            EntityDamageByEntityEvent attack = (EntityDamageByEntityEvent) damageEvent;
-            if (attack.getDamager() instanceof Player) {
-                Player damager = (Player) attack.getDamager();
-                Player victim = e.getEntity();
-                String uuid = damager.getUniqueId().toString();
-                String victimUUID = victim.getUniqueId().toString();
-                MGPlayer mgp = Races.getMGPlayer(damager);
-                if (damager == victim) 
-             	   ChatUtil.sendString(damager, "No PerkPoints for you :(");
-                else
-                process(uuid, victimUUID, mgp, damager);
-            } else if (attack.getDamager() instanceof Arrow && ((Arrow) attack.getDamager()).getShooter() instanceof Player) {
-                Player damager = (Player) ((Arrow) attack.getDamager()).getShooter();
-                Player victim = e.getEntity();
-                String uuid = damager.getUniqueId().toString();
-                String victimUUID = victim.getUniqueId().toString();
-                MGPlayer mgp = Races.getMGPlayer(damager);
+		EntityDamageEvent damageEvent = e.getEntity().getLastDamageCause();
+		if (damageEvent != null
+				&& damageEvent instanceof EntityDamageByEntityEvent) {
+			EntityDamageByEntityEvent attack = (EntityDamageByEntityEvent) damageEvent;
+			if (attack.getDamager() instanceof Player) {
+				Player damager = (Player) attack.getDamager();
+				Player victim = e.getEntity();
+				String uuid = damager.getUniqueId().toString();
+				String victimUUID = victim.getUniqueId().toString();
+				MGPlayer mgp = Races.getMGPlayer(damager);
+				if (damager == victim) {
 
-                process(uuid, victimUUID, mgp, damager);
-            }
-        }
-    }
+				} else
+					process(uuid, victimUUID, mgp, damager);
+			} else if (attack.getDamager() instanceof Arrow
+					&& ((Arrow) attack.getDamager()).getShooter() instanceof Player) {
+				Player damager = (Player) ((Arrow) attack.getDamager())
+						.getShooter();
+				Player victim = e.getEntity();
+				String uuid = damager.getUniqueId().toString();
+				String victimUUID = victim.getUniqueId().toString();
+				MGPlayer mgp = Races.getMGPlayer(damager);
 
-    private void process(String uuid, String victimUUID, MGPlayer mgp, Player damager) {
-        if (killTracker.containsKey(uuid)) {
-            if (!hasKilled(uuid, victimUUID)) {
-                addKill(uuid, victimUUID);
-                mgp.addPerkPoints(1);
-                ChatUtil.sendString(damager, "You earned 1 Perk-Point for killing a player!");
-                return;
-            }
-            ChatUtil.sendString(damager, "You earned no Perk-Points because you already killed this player today.");
-        } else {
-            killTracker.put(uuid, new KillData(damager));
-            addKill(uuid, victimUUID);
-            mgp.addPerkPoints(1);
-            ChatUtil.sendString(damager, "You earned 1 Perk-Point!");
-        }
-    }
+				process(uuid, victimUUID, mgp, damager);
+			}
+		}
+	}
 
-    private void addKill(String attackerUUID, String victimUUID) {
-        killTracker.get(attackerUUID).addPlayerToKilled(victimUUID);
-    }
+	private void process(String uuid, String victimUUID, MGPlayer mgp,
+			Player damager) {
+		if (killTracker.containsKey(uuid)) {
+			if (!hasKilled(uuid, victimUUID)) {
+				addKill(uuid, victimUUID);
+				mgp.addPerkPoints(1);
+				ChatUtil.sendString(damager,
+						"You earned 1 Perk-Point for killing a player!");
+				return;
+			}
+			ChatUtil.sendString(damager,
+					"You earned no Perk-Points because you already killed this player today.");
+		} else {
+			killTracker.put(uuid, new KillData(damager));
+			addKill(uuid, victimUUID);
+			mgp.addPerkPoints(1);
+			ChatUtil.sendString(damager, "You earned 1 Perk-Point!");
+		}
+	}
 
-    private boolean hasKilled(String attackerUUID, String victimUUID) {
-        return killTracker.get(attackerUUID).hasKilled(victimUUID);
-    }
+	private void addKill(String attackerUUID, String victimUUID) {
+		killTracker.get(attackerUUID).addPlayerToKilled(victimUUID);
+	}
+
+	private boolean hasKilled(String attackerUUID, String victimUUID) {
+		return killTracker.get(attackerUUID).hasKilled(victimUUID);
+	}
 
 }
