adbc7cd7cc7649fb4bf94333df59853adf1d965f
Jan <janie177@hotmail.com>
2015-07-18 18:24:13 +0200

    Removed races from donor world.

--- src/main/java/com/minegusta/mgracesredone/util/WorldCheck.java
+++ src/main/java/com/minegusta/mgracesredone/util/WorldCheck.java
@@ -6,7 +6,7 @@ import org.bukkit.World;
 import java.util.List;
 
 public class WorldCheck {
-    private static List<String> enabledWorlds = Lists.newArrayList("world", "world_nether", "world_the_end", "donor");
+    private static List<String> enabledWorlds = Lists.newArrayList("world", "world_nether", "world_the_end");
 
     public static boolean isEnabled(String world) {
         return enabledWorlds.contains(world);
