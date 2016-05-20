882fad1efe1c347b57f5d7e99c53aa0e2ece15d4
Jan <janie177@hotmail.com>
2016-05-20 22:55:25 +0200

    Added effect to Divine Blade.

--- src/main/java/com/minegusta/mgracesredone/races/skilltree/abilities/perks/angel/DivineBlade.java
+++ src/main/java/com/minegusta/mgracesredone/races/skilltree/abilities/perks/angel/DivineBlade.java
@@ -7,10 +7,13 @@ import com.minegusta.mgracesredone.playerdata.MGPlayer;
 import com.minegusta.mgracesredone.races.RaceType;
 import com.minegusta.mgracesredone.races.skilltree.abilities.AbilityType;
 import com.minegusta.mgracesredone.races.skilltree.abilities.IAbility;
+import com.minegusta.mgracesredone.util.EffectUtil;
 import com.minegusta.mgracesredone.util.PotionUtil;
 import com.minegusta.mgracesredone.util.WeaknessUtil;
 import org.bukkit.Bukkit;
+import org.bukkit.Effect;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.potion.PotionEffect;
@@ -52,6 +55,8 @@ public class DivineBlade implements IAbility {
 			Bukkit.getScheduler().scheduleSyncDelayedTask(Main.getPlugin(), () -> {
 				if (!player.isOnline()) return;
 
+				player.getWorld().spigot().playEffect(player.getLocation(), Effect.VILLAGER_PLANT_GROW, 0, 0, 1, 1, 1, 1, 12, 40);
+				EffectUtil.playSound(player, Sound.ENTITY_EXPERIENCE_ORB_PICKUP);
 				WeaknessUtil.removeWeakness(player);
 				for (PotionEffect e : player.getActivePotionEffects()) {
 					if (PotionUtil.isNegativeForPlayer(e.getType())) {
